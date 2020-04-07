3a12ff56c9c2cda597f7ba204db106eaa609c5e8
Rachel <>
2020-04-07 17:37:40 -0400

    to_csv allows optional parameter to save as a local ile

--- iConsultancy/to_csv.py
+++ iConsultancy/to_csv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def json_to_csv(responsejson):
+def json_to_csv(responsejson, filename=None):
     try:
         json_norm = pd.json_normalize(responsejson, record_path="deals")
-        return json_norm.to_csv()
+        return json_norm.to_csv(filename)
     except TypeError:
         raise Exception("json_to_csv uses a JSON object")
