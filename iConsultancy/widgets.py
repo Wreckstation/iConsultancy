7dd143ec24f8aa2e9430cfd1074e8fcb546412ff
Rachel <>
2020-04-14 12:19:27 -0400

    More filtering option buttons

--- iConsultancy/widgets.py
+++ iConsultancy/widgets.py
@@ -2,6 +2,7 @@
 from IPython.display import display, Markdown, clear_output
 # widget packages
 import ipywidgets as widgets # defining some widgets
+from ipywidgets import Layout
 
 from iConsultancy.get_deals import request
 from iConsultancy.to_csv import json_to_csv
@@ -24,12 +25,13 @@ slider = widgets.FloatSlider(
          step=0.1,)
 
 search_field_menu = widgets.Dropdown(
-       options=[('All', 'all'), ('Title', 'title'), ('Contacts', 'contact'), ('Organizations', 'org')],
-       value='all',
-       description='Search:')
+        options=[('All', 'all'), ('Title', 'title'), ('Contacts', 'contact'), ('Organizations', 'org')],
+        value='all',
+        description='Search:',
+        layout=Layout(width = '15em'))
 search_text = widgets.Text(
        placeholder='Search here',
-       description=':')
+       description='')
 status_menu = widgets.Dropdown(
        options=[('Any', ''), ('Open', 0), ('Won', 1), ('Lost', 2)],
        value='',
@@ -37,17 +39,47 @@ status_menu = widgets.Dropdown(
 stage_text = widgets.Text(
        placeholder='',
        description='Deal Stage:')
+owner_text = widgets.Text(
+       placeholder='',
+       description='Owner:')
+
+nextdate_range_menu = widgets.Dropdown(
+       options=[('Any', ''),('Upcoming', 'upcoming'), ('Scheduled', 'scheduled'), ('Overdue', 'overdue'), ('No tasks', 'no-task')],
+       value='',
+       description='Next Date:')
+
+tag_text = widgets.Text(
+       placeholder='',
+       description='') #TODO: tag id must be known
+
+tasktype_text = widgets.Text(
+       placeholder='',
+       description='Task type:')
+
+minval_floattext = widgets.FloatText(
+       placeholder='0',
+       description='',
+       layout=Layout(width = '20%'))
+maxval_floattext = widgets.FloatText(
+       placeholder='0',
+       description='',
+       layout=Layout(width = '20%'))
+val_label = widgets.Label(
+    value='<= Value <=')
+
+val_box = widgets.HBox([minval_floattext, val_label, maxval_floattext])
 
 checkbox = widgets.Checkbox(
-           description='return as CSV',
-           value=True)
+           description='return as JSON',
+           value=False)
 
 #Choose date
 tempbox = widgets.HBox([calendar, calendar])
 
 #search term
 box = widgets.VBox([text, slider, tempbox, checkbox ])
-search_box = widgets.Box(children=[search_field_menu, search_text], layout=widgets.Layout(justify_content="flex-start"))
+search_box = widgets.Box(children=[search_field_menu, widgets.Label(
+    value=':'), search_text], layout=widgets.Layout(justify_content="flex-start"))
 
 button = widgets.Button(description='My Button')
 out = widgets.Output()
@@ -63,7 +95,7 @@ def on_button_clicked(_):
                      'status': status_menu.value,
                      'stage': stage_text.value}
           response = request(filters)
-          if checkbox.value == True:
+          if checkbox.value == False:
             a = json_to_csv(response.json())
           else:
             a = response.text
@@ -74,4 +106,4 @@ button.on_click(on_button_clicked)
 
 button_with_check = widgets.HBox([button, checkbox])
 #final search menu
-full_display = widgets.VBox([search_box, status_menu, stage_text, button_with_check,out])
+full_display = widgets.VBox([search_box, status_menu, stage_text, val_box, button_with_check,out])
