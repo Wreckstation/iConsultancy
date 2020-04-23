ee545de074390a9bd31baad6cb23d127bddf1261
andezgrip <powergrip776@gmail.com>
2020-04-23 16:59:52 -0400

    Revert "Added sort/order"
    This reverts commit 8e414923ffceab6601bc1fb5aa8bdda4d326f7a2.

--- iConsultancy/get_deals.py
+++ iConsultancy/get_deals.py
@@ -9,19 +9,26 @@ headers = {'Api-Token': config["KEY"]}
 s = requests.Session()
 s.headers.update(headers)
 
-def request_deals(filters, sort):
-    order = sort['sortby'] 
-    # Request a dictionary of deals using the specified filters and sort order.
+def request_deals(filters):
+    # Request a dictionary of deals using the specified filters.
     payload = {'filters[search_field]': filters['search_field'],
                'filters[search]': filters['search'],
                'filters[status]': filters['status'],
                'filters[stage]': filters['stage'],
                'filters[owner]': filters['owner'],
+               'filters[nextdate_range]': filters['nextdate_range'],
                'filters[tag]': filters['tag'],
                'filters[tasktype]': filters['tasktype'],
-               'filters[nextdate_range]': filters['nextdate_range'],
-               f'orders[{order}]': sort['sortorder']
-               }  
+               'filters[created_before]': filters['created_before'],
+               'filters[created_after]': filters['created_after'],
+               'filters[updated_before]': filters['updated_before'],
+               'filters[updated_after]': filters['updated_after'],
+               'filters[minimum_value]': filters['minimum_value'],
+               'filters[maximum_value]': filters['maximum_value'],
+               'filters[score_greater_than]': filters['score_greater_than'],
+               'filters[score_less_than]': filters['score_less_than'],
+               'filters[score]': filters['score']
+               }
     url = config["URL"] + "deals"
     response = s.get(url, params=payload)
     response.close() # Close connection to server
@@ -56,7 +63,7 @@ def return_tag_ids(search, b_multiple_tag_search = False):
     else:
         return [1, [response['tags'][0]['id']]]
 
-def request(filters, sort, b_output_type = True, fn = None, mt = False):
+def request(filters, b_output_type = True, fn = None, mt = False):
     # Prepare tag id search by converting name to ids
     req_filters = filters
     req_filters['tag'] = ''
@@ -77,11 +84,11 @@ def request(filters, sort, b_output_type = True, fn = None, mt = False):
         for tag in tags: # If searching by multiple tags, search all and concat into one df
             req_filters['tag'] = tag
             if result is None:
-                result = to_csv.json_to_df(request_deals(req_filters, sort).json())
+                result = to_csv.json_to_df(request_deals(req_filters).json())
             else:
-                result += to_csv.json_to_df(request_deals(req_filters, sort).json())
+                result += to_csv.json_to_df(request_deals(req_filters).json())
     else:
-        result = to_csv.json_to_df(request_deals(req_filters, sort).json())
+        result = to_csv.json_to_df(request_deals(req_filters).json())
     # Return values based on output type: True for JSON, False for CSV
     if b_output_type:
         return [result.to_json(), notices]
--- iConsultancy/widgets.py
+++ iConsultancy/widgets.py
@@ -6,23 +6,7 @@ from ipywidgets import Layout
 
 from iConsultancy.get_deals import request
 
-text = widgets.Text(
-       value='My Text',
-       description='Title', )
-
-calendar = widgets.DatePicker(
-           description='Select Date')
-calendar1 = widgets.DatePicker(
-           description='From Date')
-calendar2 = widgets.DatePicker(
-           description='To Date')
-
-slider = widgets.FloatSlider(
-         value=1,
-         min=0,
-         max=10.0,
-         step=0.1,)
-
+# Search field
 search_field_menu = widgets.Dropdown(
         options=[('All', 'all'), ('Title', 'title'), ('Contacts', 'contact'), ('Organizations', 'org')],
         value='all',
@@ -31,22 +15,28 @@ search_field_menu = widgets.Dropdown(
 search_text = widgets.Text(
        placeholder='Search here',
        description='')
+
+# Status
 status_menu = widgets.Dropdown(
        options=[('Any', ''), ('Open', 0), ('Won', 1), ('Lost', 2)],
        value='',
        description='Status:')
+# Stage
 stage_text = widgets.Text(
        placeholder='',
        description='Deal Stage:')
+# Owner
 owner_text = widgets.Text(
        placeholder='',
        description='Owner:')
 
+# Next date range
 nextdate_range_menu = widgets.Dropdown(
        options=[('Any', ''),('Upcoming', 'upcoming'), ('Scheduled', 'scheduled'), ('Overdue', 'overdue'), ('No tasks', 'no-task')],
        value='',
        description='Next Date:')
 
+# Tags
 tag_text = widgets.Text(
        placeholder='',
        description='Tag Name:')
@@ -55,20 +45,29 @@ multiple_tag_checkbox = widgets.Checkbox(
            value=False)
 tag_box = widgets.HBox([tag_text, multiple_tag_checkbox])
 
+# Tasktype
 tasktype_text = widgets.Text(
        placeholder='',
        description='Task type:')
+# Created
+created_label = widgets.Label(value='Created')
+created_before_calendar = widgets.DatePicker(
+           description='Before:')
+created_after_calendar = widgets.DatePicker(
+           description='After:')
+created_hbox = widgets.HBox([created_before_calendar, created_after_calendar])
+created_box = widgets.VBox([created_label, created_hbox])
 
-sortby_menu = widgets.Dropdown(
-       options=[('N/A', ''), ('Title', 'title'), ('Value', 'value'), ('Created date', 'cdate'), ('Primary contact first name', 'contact_name'), ('Primary contact organization name', 'contact_orgname'), ('Next task due date', 'next-action')],
-       value='',
-       description='Sort by:',
-       layout=Layout(width = '15em'))   
-sortorder_menu = widgets.Dropdown(
-       options=[('Descending', 'DESC'), ('Ascending', 'ASC')],
-       value='DESC',
-       description='')
+# Updated
+updated_label = widgets.Label(value='Updated')
+updated_before_calendar = widgets.DatePicker(
+           description='Before:')
+updated_after_calendar = widgets.DatePicker(
+           description='After:')
+updated_hbox = widgets.HBox([updated_before_calendar, updated_after_calendar])
+updated_box = widgets.VBox([updated_label, updated_hbox])
 
+# Minimum and maximum value
 minval_floattext = widgets.FloatText(
        placeholder='0',
        description='',
@@ -78,9 +77,24 @@ maxval_floattext = widgets.FloatText(
        description='',
        layout=Layout(width = '20%'))
 val_label = widgets.Label(value='≤ Value ≤')
-
 val_box = widgets.HBox([minval_floattext, val_label, maxval_floattext])
 
+# Score
+score_floatext = widgets.FloatText(
+    placeholder='0',
+    description='Score =')
+score_g_floattext = widgets.FloatText(
+       placeholder='0',
+       description='',
+       layout=Layout(width = '20%'))
+score_l_floattext = widgets.FloatText(
+       placeholder='0',
+       description='',
+       layout=Layout(width = '20%'))
+score_label = widgets.Label(value='< Score <')
+score_box = widgets.HBox([score_g_floattext, score_label, score_l_floattext])
+
+# Output options
 checkbox = widgets.Checkbox(
            description='return as JSON (debug)',
            value=True)
@@ -94,20 +108,12 @@ file_name_text = widgets.Text(
     layout=Layout(width = '50%'),
     value='output.csv')
 
-#Choose date
-tempbox = widgets.HBox([calendar, calendar])
-
 #search term
-box = widgets.VBox([text, slider, tempbox, checkbox ])
 search_box = widgets.Box(children=[search_field_menu, widgets.Label(
     value=':'), search_text], layout=widgets.Layout(justify_content="flex-start"))
 
-#sort
-box2 = widgets.VBox([text, slider, tempbox, checkbox ])
-sort_box = widgets.Box(children=[sortby_menu, widgets.Label(
-    value='in this order:'), sortorder_menu], layout=widgets.Layout(justify_content="flex-start"))
-
-button = widgets.Button(description='Get .CSV Report')
+button = widgets.Button(description='Get .CSV Report',
+                        layout=Layout(width = '50%'))
 out = widgets.Output()
 
 # Output widgets
@@ -123,17 +129,39 @@ def on_button_clicked(_):
                    'owner': owner_text.value,
                    'tags': tag_text.value,
                    'tasktype': tasktype_text.value,
-                   'nextdate_range': nextdate_range_menu.value
+                   'created_before': created_before_calendar.value,
+                   'created_after': created_after_calendar.value,
+                   'updated_before': updated_before_calendar.value,
+                   'updated_after': updated_after_calendar.value,
+                   'nextdate_range': nextdate_range_menu.value,
+                   'minimum_value': minval_floattext.value,
+                   'maximum_value': maxval_floattext.value,
+                   'score_greater_than': score_g_floattext.value,
+                   'score_less_than': score_l_floattext.value,
+                   'score': scorscore_floatext.value
                   }
-        sort = {'sortby': sortby_menu.value, 'sortorder': sortorder_menu.value}
-        response = request(filters, sort, checkbox.value, file_name_text.value, multiple_tag_checkbox.value)
+        response = request(filters, checkbox.value, file_name_text.value, multiple_tag_checkbox.value)
         if warning_checkbox.value == True:
             print(response[1]) # return any system messages that the query returned
         print(response[0])
 
 # linking button and function together using a button's method
 button.on_click(on_button_clicked)
-
 button_with_check = widgets.HBox([button, checkbox, warning_checkbox])
+
 #final search menu
-full_display = widgets.VBox([search_box, status_menu, stage_text, owner_text, tag_box, nextdate_range_menu, tasktype_text, sort_box, val_box, button_with_check,file_name_text,out])
+full_display = widgets.VBox([search_box,
+                            status_menu,
+                            stage_text,
+                            owner_text, 
+                            tag_box, 
+                            nextdate_range_menu, 
+                            tasktype_text, 
+                            created_box, 
+                            updated_box, 
+                            val_box, 
+                            score_floatext,
+                            score_box, 
+                            button_with_check,
+                            file_name_text,
+                            out])
